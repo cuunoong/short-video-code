0a1e41eee0514d84fa98d82bfad2789dfac1913a
Arif Iskandar <arif19iskandar@gmail.com>
2022-12-10 10:18:00 +0700

    Adding javascript short code

---
+++ javascript/decimal.js
@@ -0,0 +1,7 @@
+const number = 19.80974587387385783;
+
+// 19.8097
+console.log(number.toFixed(4));
+
+// 19.81
+console.log(number.toFixed(2));
---
+++ javascript/destructing-object.js
@@ -0,0 +1,12 @@
+const user = {
+  name: "Arif Iskandar",
+  role: "Developer",
+};
+
+// const name = user.name;
+// const role = user.role;
+
+// console.log(name, role);
+
+const { name, role } = user;
+console.log(name, role);
